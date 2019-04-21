2dcb150393010fa905bbf09486a6a1a80adc7492
Ion Adrian <ion.adrian@live.com>
2019-04-21 20:49:31 +0100

    added POJO

---
+++ src/main/java/net/comdava/petproject/model/Person.java
@@ -0,0 +1,14 @@
+package net.comdava.petproject.model;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    // getters
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+
+    // setters
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+}
---
+++ src/main/java/net/comdava/petproject/model/PetType.java
@@ -0,0 +1,11 @@
+package net.comdava.petproject.model;
+
+public class PetType {
+    private String name;
+
+    // getters
+    public String getName() { return name; }
+
+    // setters
+    public void setName(String name) { this.name = name; }
+}
