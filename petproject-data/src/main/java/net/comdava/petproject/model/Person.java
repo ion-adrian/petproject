dda943cfe68b7c9ce4d9c33bfddeea1eb08a4aa7
Ion Adrian <ion.adrian@live.com>
2019-04-22 03:42:11 +0100

    BaseEntity added to data model

--- petproject-data/src/main/java/net/comdava/petproject/model/Person.java
+++ petproject-data/src/main/java/net/comdava/petproject/model/Person.java
@@ -1,6 +1,6 @@
 package net.comdava.petproject.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- petproject-data/src/main/java/net/comdava/petproject/model/PetType.java
+++ petproject-data/src/main/java/net/comdava/petproject/model/PetType.java
@@ -1,6 +1,6 @@
 package net.comdava.petproject.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     // getters
