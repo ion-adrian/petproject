fc3d253f26c8c47ff9bd5cad4c30aada0cd6827e
Ion Adrian <ion.adrian@live.com>
2019-04-24 23:06:25 +0100

    vets controller and html page added; close #9

--- petproject-data/src/main/java/net/comdava/petproject/service/VetService.java
+++ petproject-data/src/main/java/net/comdava/petproject/service/VetService.java
@@ -3,5 +3,5 @@ package net.comdava.petproject.service;
 import net.comdava.petproject.model.Vet;
 
 
-public interface VetService extends net.comdava.petproject.service.CrudService<Vet,Long> {
+public interface VetService extends CrudService<Vet,Long> {
 }
