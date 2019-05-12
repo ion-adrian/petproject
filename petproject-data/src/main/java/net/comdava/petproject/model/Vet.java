950a28f88037cd4d4dadfd0e67c6a5aa2af67f94
Ion Adrian <ion.adrian@live.com>
2019-05-12 12:44:24 +0100

    Specialty added, Vet modified to accept Specialties; close #25

--- petproject-data/src/main/java/net/comdava/petproject/model/Vet.java
+++ petproject-data/src/main/java/net/comdava/petproject/model/Vet.java
@@ -1,4 +1,16 @@
 package net.comdava.petproject.model;
 
+import java.util.Set;
+
 public class Vet extends net.comdava.petproject.model.Person {
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
