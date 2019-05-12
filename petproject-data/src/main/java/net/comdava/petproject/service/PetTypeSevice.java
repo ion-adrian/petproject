2f4ac82509dc5882750e1f0ddb2ba0c09a571ca9
Ion Adrian <ion.adrian@live.com>
2019-05-12 14:32:23 +0100

    PetTypeService interface added which extends CrudService, PetTypeMapService class added; close #27

---
+++ petproject-data/src/main/java/net/comdava/petproject/service/PetTypeSevice.java
@@ -0,0 +1,6 @@
+package net.comdava.petproject.service;
+
+import net.comdava.petproject.model.PetType;
+
+public interface PetTypeSevice extends CrudService<PetType, Long> {
+}
---
+++ petproject-data/src/main/java/net/comdava/petproject/service/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package net.comdava.petproject.service.map;
+
+import net.comdava.petproject.model.PetType;
+import net.comdava.petproject.service.PetTypeSevice;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType,Long> implements PetTypeSevice {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteByID(id);
+    }
+}
